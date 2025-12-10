baff251145ee172cb2a9b10643c853d0c14be693
Ivan Vallejo <157355377+IvanDeIvam@users.noreply.github.com>
2025-12-10 22:01:18 +0100

    Exercici 7 gota i acabat

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 option = 0
-x = 0
-y = 0
+x = 2
+y = 2
 
 def mostrar_temperatura():
     led.plot_bar_graph(input.temperature(), 50)
 
 def gota():
-    pass
+    global x, y
+    oldX = x
+    oldY = y
+
+    accX = input.acceleration(Dimension.X)
+    accY = input.acceleration(Dimension.Y)
+
+    if accX < -150 and x > 0:
+        x = x - 1
+    if accX > 150 and x < 4:
+        x = x + 1
+
+    if accY < -150 and y > 0:
+        y = y - 1
+    if accY > 150 and y < 4:
+        y = y + 1
+
+    if x != oldX or y != oldY:
+        led.unplot(oldX, oldY)
+
+    led.plot(x, y)
+    basic.pause(50)
+
 def on_button_pressed_a():
     global option
     option = 1
+    basic.clear_screen()
     pass
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     global option
     option = 2
+    basic.clear_screen()
     pass
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
--- main.ts
+++ main.ts
@@ -1,22 +1,50 @@
 let option = 0
-let x = 0
-let y = 0
+let x = 2
+let y = 2
 function mostrar_temperatura() {
     led.plotBarGraph(input.temperature(), 50)
 }
 
 function gota() {
     
+    let oldX = x
+    let oldY = y
+    let accX = input.acceleration(Dimension.X)
+    let accY = input.acceleration(Dimension.Y)
+    if (accX < -150 && x > 0) {
+        x = x - 1
+    }
+    
+    if (accX > 150 && x < 4) {
+        x = x + 1
+    }
+    
+    if (accY < -150 && y > 0) {
+        y = y - 1
+    }
+    
+    if (accY > 150 && y < 4) {
+        y = y + 1
+    }
+    
+    if (x != oldX || y != oldY) {
+        led.unplot(oldX, oldY)
+    }
+    
+    led.plot(x, y)
+    basic.pause(50)
 }
 
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     option = 1
+    basic.clearScreen()
     
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
     option = 2
+    basic.clearScreen()
     
 })
 basic.forever(function on_forever() {

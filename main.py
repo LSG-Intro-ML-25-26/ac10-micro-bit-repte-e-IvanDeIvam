9a643a946a1f247a83440a37f91012bc3d77d72c
Ivan Vallejo <157355377+IvanDeIvam@users.noreply.github.com>
2025-12-10 21:56:49 +0100

    Funciones separadas por a y b

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-def on_forever():
+option = 0
+x = 0
+y = 0
+
+def mostrar_temperatura():
     led.plot_bar_graph(input.temperature(), 50)
+
+def gota():
     pass
+def on_button_pressed_a():
+    global option
+    option = 1
+    pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global option
+    option = 2
+    pass
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_forever():
+    if option == 1:
+        mostrar_temperatura()
+    if option == 2:
+        gota()
+    basic.pause(100)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,32 @@
-basic.forever(function on_forever() {
+let option = 0
+let x = 0
+let y = 0
+function mostrar_temperatura() {
     led.plotBarGraph(input.temperature(), 50)
+}
+
+function gota() {
     
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    option = 1
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    option = 2
+    
+})
+basic.forever(function on_forever() {
+    if (option == 1) {
+        mostrar_temperatura()
+    }
+    
+    if (option == 2) {
+        gota()
+    }
+    
+    basic.pause(100)
 })
